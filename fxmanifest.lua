3f9280d262c62d37fc115cc5f23baa33d53eaf9e
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-04-23 22:32:52 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Gamingluke1337 github.com/GamingLuke1337'
+description 'Bridge to lib.notify'
+
+client_scripts {
+    'esx_notify.lua',
+    'pNotify.lua',
+    'okokNotify.lua'
+}
+
+shared_script '@ox_lib/init.lua'
+
+exports {
+    'Notify',
+    'Alert',
+    'SendNotification'
+}
+
+provides {
+    'esx_notify',
+    'pNotify',
+    'okokNotify'
+}
