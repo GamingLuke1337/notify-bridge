35049a249d24325595ffc4ff99ea3348492bf169
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-05-09 21:17:02 +0200

    Update v42-notfiy.lua
    remove unnecessary comments

--- bridge/v42-notfiy.lua
+++ bridge/v42-notfiy.lua
@@ -1,9 +1,9 @@
 exports('notify', function(text, texttype, length)
     lib.notify({
-        title = "Notification", -- v42-notify scheint kein eigenes Title zu haben, deswegen nehmen wir den Typ
+        title = "Notification",
         description = text or "",
-        type = texttype or "info",           -- success, error, info, warning, etc.
-        duration = length or 5000,           -- time in ms
-        position = 'topRight',               -- kannst du anpassen, v42-notify definiert das nicht extra
+        type = texttype or "info", -- success, error, info, warning, etc.
+        duration = length or 5000, -- time in ms
+        position = 'topRight',
     })
 end)
